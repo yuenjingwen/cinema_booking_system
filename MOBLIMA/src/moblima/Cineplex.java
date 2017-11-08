6dfca5fdb04aeb6425f6d86cea604e1a43f367b8
jirehcwe <jireh_cwe@hotmail.com>
2017-11-08 11:32:18 +0800

    added attribute numOfCinema to cineplex class, added setter, changed constructor.

--- MOBLIMA/src/moblima/Cineplex.java
+++ MOBLIMA/src/moblima/Cineplex.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 public class Cineplex implements Serializable{
 	
 	private static final long serialVersionUID = 3346347368664976796L;
+	private int numOfCinema = 3;
 	private String name;
 	private ArrayList<Cinema> cinemaList;
 	
@@ -13,14 +14,20 @@ public class Cineplex implements Serializable{
 		return name;
 	}
 	
-	public Cineplex(String name) {
+	public Cineplex(String name, int num) {
 		this.name = name;
+		numOfCinema = num;
 		cinemaList = new ArrayList<Cinema>();
 	}
 	
 	public void setName(String name) {
 		this.name = name;
+		}
+	
+	public void setNumOfCinema(int num) {
+		numOfCinema = num;
 	}
+	
 	public ArrayList<Cinema> getCinemaList() {
 		return cinemaList;
 	}	
