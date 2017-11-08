82e51c05371f2cd9b585903e4e91c5aaa865f8a6
jirehcwe <jireh_cwe@hotmail.com>
2017-11-08 11:24:06 +0800

    changed Ticket constructor, added relevant attributes to Ticket class, added getters for attributes (no setters, all set in constructor)

--- MOBLIMA/src/moblima/Ticket.java
+++ MOBLIMA/src/moblima/Ticket.java
@@ -1,22 +1,24 @@
 package moblima;
 
+import java.time.LocalDateTime;
+
 public class Ticket {
 	
 	
 	private float price;
 	private String discount = "None";
-
-	public float getTicketPrice() {
-		return price;
-	}
-
-	public String getDiscount() {
-		return discount;
-	}
-
-
-
-	public Ticket(CinemaEnum cEnum, MovieType mEnum, int age, int movieDate, boolean student){
+	private CinemaEnum cinemaTier;
+	private MovieType movieType;
+	private int ticketholderAge;
+	private LocalDateTime ticketDate;
+	
+public Ticket(CinemaEnum cEnum, MovieType mEnum, int age, LocalDateTime movieDate, boolean student){
+		
+		cinemaTier = cEnum;
+		movieType = mEnum;
+		ticketholderAge = age;
+		ticketDate = movieDate;
+		
 		
 		float temp = 0;
 		
@@ -49,15 +51,44 @@ public class Ticket {
 		
 		if(age < 12){
 			discount = "Child Discount";
+			student = false;
 			temp -= 2;
 		}else if(age > 65){
 			discount = "Elderly Discount";
+			student = false;
 			temp -= 4;
 		}else if(student){
 			discount = "Student Discount";
+			student = true;
 			temp -= 2;
 		}
 		
+		price = temp;
 		
 	}
+
+	public float getTicketPrice() {
+		return price;
+	}
+
+	public String getDiscount() {
+		return discount;
+	}
+
+	public CinemaEnum getCinemaTier() {
+		return cinemaTier;
+	}
+	
+	public MovieType getMovieType() {
+		return movieType;
+	}
+	
+	public int getTicketholderAge() {
+		return ticketholderAge;
+	}
+	
+	public LocalDateTime getTicketDate() {
+		return ticketDate;
+	}
+
 }
