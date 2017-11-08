1cef01f76839a08502293f9087c272db58d31355
Johann <Johann@10.27.34.2>
2017-11-08 23:55:42 +0800

    Previous version got error. Taken the error in customer module out- johann

--- MOBLIMA/src/moblima/CustomerModule.java
+++ MOBLIMA/src/moblima/CustomerModule.java
@@ -111,7 +111,6 @@ public class CustomerModule {
 		movieIndex = scanner.nextInt();
 		scanner.nextLine();
 		ticket.movie=CineplexDatabase.cineplexList.get(cineplexIndex -1).getCinemaList().get(cinemaIndex-1).getCinemaShowList().get(movieIndex-1).getMovie().getTitle();
-		CineplexDatabase.cineplexList.get(cineplexIndex -1).getCinemaList().get(cinemaIndex-1).getCinemaShowList().get(index);
 		/*i = 1;
 		System.out.println("\n===============================");
 		for(Cinema c : CineplexDatabase.cineplexList.get(cineplexIndex -1).getCinemaList().get(cinemaIndex-1).getCinemaShowList().get(movieIndex-1).getShowtime()){
