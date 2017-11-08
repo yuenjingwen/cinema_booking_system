e81d4cb9f6fe74794dc76b903a59433c8a7695e0
jirehcwe <jireh_cwe@hotmail.com>
2017-11-08 11:02:05 +0800

    Added IStorage interface and added 4 database classes

---
+++ MOBLIMA/src/moblima/IStorage.java
@@ -0,0 +1,10 @@
+package moblima;
+
+public interface IStorage {
+	
+	public abstract void add();
+	
+	public abstract void remove();
+	
+	public abstract void edit();
+}
---
+++ MOBLIMA/src/moblima/ReviewDatabase.java
@@ -0,0 +1,23 @@
+package moblima;
+
+public class ReviewDatabase implements IStorage {
+
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void remove() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void edit() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
