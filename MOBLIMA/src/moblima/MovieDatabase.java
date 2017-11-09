1fd065742a1d46cf04c87bc00332b63a68ebf75c
YuenJingWen <YuenJingWen@172.22.208.142>
2017-11-09 12:50:45 +0800

    edited movie database

--- MOBLIMA/src/moblima/MovieDatabase.java
+++ MOBLIMA/src/moblima/MovieDatabase.java
@@ -222,8 +222,8 @@ public class MovieDatabase{
 	}
 	public static void addReview(String reviewText, int rating) {
 		MovieReview temp = new MovieReview(reviewText, rating);
+		RDB.add(temp);
 		
-			
 		File file = new File("MovieReview.dat");
 		try{
 			FileOutputStream fo = new FileOutputStream(file);
@@ -240,7 +240,7 @@ public class MovieDatabase{
 		}
 	}
 
-	public void remove(int reviewIndex) {
+	public void removeReview(int reviewIndex) {
 		RDB.remove(reviewIndex-1);
 		
 		File file = new File("MovieReview.dat");
@@ -261,7 +261,7 @@ public class MovieDatabase{
 
 	
 
-	public void edit(int reviewIndex, String reviewText, int rating) {
+	public void editReview(int reviewIndex, String reviewText, int rating) {
 		MovieReview temp = new MovieReview(reviewText, rating);
 		RDB.set(reviewIndex, temp);
 		
@@ -287,6 +287,7 @@ public class MovieDatabase{
 		System.out.print("Which movie review would you like to read?");	//ask which movie you want to see review for
 		Scanner sc = new Scanner(System.in);
 		int choice = sc.nextInt();
+		sc.close();
 		
 		//print all reviews for that movie 
 		for (MovieReview r : MovieDatabase.getArrayList().get(choice).getReviewList()) {	//print into format
@@ -298,83 +299,9 @@ public class MovieDatabase{
 	
 
 	
-	public static void addReview(String reviewText, int rating) {
-		MovieReview temp = new MovieReview(reviewText, rating);
-		
-			
-		File file = new File("MovieReview.dat");
-		try{
-			FileOutputStream fo = new FileOutputStream(file);
-			ObjectOutputStream output = new ObjectOutputStream(fo);
-			for(MovieReview r: RDB){
-				output.writeObject(r);
-			}
-			fo.close();
-			output.close();
-		} catch (FileNotFoundException e){
-			e.printStackTrace();
-		} catch (IOException e){
-			e.printStackTrace();
-		}
-	}
-
-	public void remove(int reviewIndex) {
-		RDB.remove(reviewIndex-1);
-		
-		File file = new File("MovieReview.dat");
-		try{
-			FileOutputStream fo = new FileOutputStream(file);
-			ObjectOutputStream output = new ObjectOutputStream(fo);
-			for(MovieReview r: RDB){
-				output.writeObject(r);
-			}
-			fo.close();
-			output.close();
-		} catch (FileNotFoundException e){
-			e.printStackTrace();
-		} catch (IOException e){
-			e.printStackTrace();
-		}
-	}
-
 	
 
-	public void edit(int reviewIndex, String reviewText, int rating) {
-		MovieReview temp = new MovieReview(reviewText, rating);
-		RDB.set(reviewIndex, temp);
-		
-		File file = new File("MovieReview.dat");
-		try{
-			FileOutputStream fo = new FileOutputStream(file);
-			ObjectOutputStream output = new ObjectOutputStream(fo);
-			for(MovieReview r: RDB){
-				output.writeObject(r);
-			}
-			fo.close();
-			output.close();
-		} catch (FileNotFoundException e){
-			e.printStackTrace();
-		} catch (IOException e){
-			e.printStackTrace();
-		}
-	}
 	
-	public void fetchReview() {
-		
-		MovieDatabase.printMovieList();	//print movie list
-		System.out.print("Which movie review would you like to read?");	//ask which movie you want to see review for
-		Scanner sc = new Scanner(System.in);
-		int choice = sc.nextInt();
-		
-		//print all reviews for that movie 
-		for (MovieReview r : MovieDatabase.getArrayList().get(choice).getReviewList()) {	//print into format
-			System.out.println(r.getReview());
-			System.out.println(r.getRating());
-		}
-	}
-
-	
-
 }
 
 
