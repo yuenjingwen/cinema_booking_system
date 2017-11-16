823d9cbc028d8a1b648bed884f0cecb7f7d4b985
kanzijun <kanzijun@gmail.com>
2017-11-17 02:27:38 +0800

    done java doc database interface

--- MOBLIMA/src/moblima/Database.java
+++ MOBLIMA/src/moblima/Database.java
@@ -1,8 +1,15 @@
 package moblima;
 
+/**
+ * Interface to be implemented in all databases.
+ */
 public interface Database {
-	
+	/**
+	 * Updates the File that saves the data of the database.
+	 */
 	public void updateDatabase();
-	
+	/**
+	 * Fetches data from the File that saves data of the database.
+	 */
 	public void fetchDatabase();
 }
