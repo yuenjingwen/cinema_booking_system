e90fc4fda9833219be69300a7f4d6801b66656be
jirehcwe <jireh_cwe@hotmail.com>
2017-11-17 00:55:22 +0800

    extra ticket changes

--- MOBLIMA/src/moblima/AdultTicket.java
+++ MOBLIMA/src/moblima/AdultTicket.java
@@ -1,10 +1,7 @@
 package moblima;
 
-import java.time.MonthDay;
-
 public class AdultTicket extends Ticket{
 	
-	static int BASE_PRICE_TICKET = 10;
 		/**
 	 * 
 	 */
@@ -14,6 +11,8 @@ public class AdultTicket extends Ticket{
 	public AdultTicket(String seat, int age, int cinemaIndex, int cineplexIndex, int showtimeIndex,
 			String ticketholderName, String email, int mobileNum) {
 		super(seat, age, cinemaIndex, cineplexIndex, showtimeIndex, ticketholderName, email, mobileNum);
+		
+		super.setTicketPrice(this.calculateTicketPrice());
 	}
 
 	@Override
@@ -21,112 +20,7 @@ public class AdultTicket extends Ticket{
 		
 		float temp = 0;
 		
-		switch(super.getCinemaType()){
-		case DIGITAL:
-			try {
-			temp += TicketDatabase.searchDiscountByName("Digital");
-			} catch (Exception e) {
-				e.printStackTrace();
-				System.out.println(e.getMessage());
-			}
-			break;
-		case PLATINIUM:
-			try {
-				temp += TicketDatabase.searchDiscountByName("Platinum");
-			} catch (Exception e) {
-				e.printStackTrace();
-				System.out.println(e.getMessage());
-			}
-			break;
-		default:
-			System.out.println("Error in ticket pricing.");
-			break;
-		}
-		
-		switch(super.getMovieType().toString()){
-		case "NORMAL":
-			try {
-				temp += TicketDatabase.searchDiscountByName("Normal");
-			} catch (Exception e) {
-				e.printStackTrace();
-				System.out.println(e.getMessage());
-			}
-			break;
-		case "BLOCKBUSTER":
-			try {
-				temp += TicketDatabase.searchDiscountByName("Blockbuster");
-			} catch (Exception e) {
-				e.printStackTrace();
-				System.out.println(e.getMessage());
-			}
-			break;
-		case "THREE_D":
-			try {
-				temp += TicketDatabase.searchDiscountByName("3D");
-			} catch (Exception e) {
-				e.printStackTrace();
-				System.out.println(e.getMessage());
-			}
-			break;
-		default:
-			System.out.println("Error in ticket pricing.");
-			break;
-		}
-		
-//		if(age < 12){
-//			discount = "Child Discount";
-//			try {
-//				temp += TicketDatabase.searchDiscountByName("Child Discount");
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//				System.out.println(e.getMessage());
-//			}
-//		}else if(age > 65){
-//			discount = "Elderly Discount";
-//			try {
-//				temp += TicketDatabase.searchDiscountByName("Elderly Discount");
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//				System.out.println(e.getMessage());
-//			}
-//		}
-		
-		switch (super.getMovieDay().getDayOfWeek().toString()){
-		case "MONDAY":
-		case "TUESDAY":
-		case "WEDNESDAY":
-		case "THURSDAY":
-		case "FRIDAY":
-			break;
-		case "SATURDAY": 
-		case "SUNDAY": try {
-				temp += TicketDatabase.searchDiscountByName("Weekend");
-			} catch (Exception e1) {
-				e1.printStackTrace();
-			}
-			break;
-		default: 
-			System.out.println("Error in ticket pricing.");
-			break;
-					
-		}
-		
-	
-		
-		//checks if the movie date lands on a public holiday and adds the appropriate charges accordingly
-		MonthDay tempMD = MonthDay.from(super.getMovieDay());
-		
-		for (PublicHoliday ph: PublicHolidayDatabase.getArrayList()) {
-			if (tempMD == ph.getDate()) {
-				try {
-					temp += TicketDatabase.searchDiscountByName("Public Holiday");
-					break;
-				} catch (Exception e) {
-					e.printStackTrace();
-					System.out.println(e.getMessage());
-				}
-			}
-		}
+		temp += super.calculateGenericTicketDiscount();
 		
 		return BASE_PRICE_TICKET + temp;
 	}
--- MOBLIMA/src/moblima/Ticket.java
+++ MOBLIMA/src/moblima/Ticket.java
@@ -1,6 +1,7 @@
 package moblima;
 
 import java.time.LocalDateTime;
+import java.time.MonthDay;
 import java.time.format.DateTimeFormatter;
 import java.io.Serializable;
 
@@ -164,6 +165,119 @@ public abstract class Ticket implements Serializable {
 	}
 	public void setSeat(String seat){
 		this.seat= seat;
+	}
+	
+	public float calculateGenericTicketDiscount() {
+			float temp = 0;
+		
+		switch(getCinemaType()){
+		case DIGITAL:
+			try {
+			temp += TicketDatabase.searchDiscountByName("Digital");
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println(e.getMessage());
+			}
+			break;
+		case PLATINIUM:
+			try {
+				temp += TicketDatabase.searchDiscountByName("Platinum");
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println(e.getMessage());
+			}
+			break;
+		default:
+			System.out.println("Error in ticket pricing.");
+			break;
+		}
+		
+		switch(getMovieType().toString()){
+		case "NORMAL":
+			try {
+				temp += TicketDatabase.searchDiscountByName("Normal");
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println(e.getMessage());
+			}
+			break;
+		case "BLOCKBUSTER":
+			try {
+				temp += TicketDatabase.searchDiscountByName("Blockbuster");
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println(e.getMessage());
+			}
+			break;
+		case "THREE_D":
+			try {
+				temp += TicketDatabase.searchDiscountByName("3D");
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println(e.getMessage());
+			}
+			break;
+		default:
+			System.out.println("Error in ticket pricing.");
+			break;
+		}
+		
+//		if(age < 12){
+//			discount = "Child Discount";
+//			try {
+//				temp += TicketDatabase.searchDiscountByName("Child Discount");
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//				System.out.println(e.getMessage());
+//			}
+//		}else if(age > 65){
+//			discount = "Elderly Discount";
+//			try {
+//				temp += TicketDatabase.searchDiscountByName("Elderly Discount");
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//				System.out.println(e.getMessage());
+//			}
+//		}
+		
+		switch (getMovieDay().getDayOfWeek().toString()){
+		case "MONDAY":
+		case "TUESDAY":
+		case "WEDNESDAY":
+		case "THURSDAY":
+		case "FRIDAY":
+			break;
+		case "SATURDAY": 
+		case "SUNDAY": try {
+				temp += TicketDatabase.searchDiscountByName("Weekend");
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+			break;
+		default: 
+			System.out.println("Error in ticket pricing.");
+			break;
+					
+		}
+		
+	
+		
+		//checks if the movie date lands on a public holiday and adds the appropriate charges accordingly
+		MonthDay tempMD = MonthDay.from(getMovieDay());
+		
+		for (PublicHoliday ph: PublicHolidayDatabase.getArrayList()) {
+			if (tempMD == ph.getDate()) {
+				try {
+					temp += TicketDatabase.searchDiscountByName("Public Holiday");
+					break;
+				} catch (Exception e) {
+					e.printStackTrace();
+					System.out.println(e.getMessage());
+				}
+			}
+		}
+		
+		return temp;
 	}
 	
 }
